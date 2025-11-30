feaf46946cd9a76f9ce9b9421662998f7f4a4049
曹琰 <894042683@qq.com>
2025-11-30 23:32:05 +0800

    将前端代码整理到 frontend 文件夹

---
+++ frontend/backend/NoodleTrail/src/main/java/com/example/noodletrail/NoodleTrailApplication.java
@@ -0,0 +1,13 @@
+package com.example.noodletrail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoodleTrailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoodleTrailApplication.class, args);
+    }
+
+}
---
+++ frontend/backend/NoodleTrail/src/test/java/com/example/noodletrail/NoodleTrailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.noodletrail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoodleTrailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
